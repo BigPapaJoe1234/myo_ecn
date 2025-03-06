11dfe39f87d6050fec9099cf54ea8924e58ad5f4
Logan Bellamy <156553133+BigPapaJoe1234@users.noreply.github.com>
2025-03-06 16:49:47 -0600

    This works

--- examples/classification/CSV_Processor.py
+++ examples/classification/CSV_Processor.py
@@ -2,11 +2,13 @@
 Author: Logan Bellamy
 Date: 03/06/2025
 
+Warning: If you are using GitHub for version control DO NOT push the input file. It is too big.
+         DO NOT push up the output files either. They are also too big. Very inconvenient. Working on a solution.
+
 This is a super inefficient CSV sorting algorithm.
 There is probably a package that could be used to speed this up.
 Maybe hashing could help organize this
 ...but you should definitely expect worst case from any sorting algorithm you might use.
-The input is large.
 
 '''
 
@@ -24,7 +26,7 @@ expected_gesture = ["tongue", "foot", "right", "left"]
 
 # Creates directories and CSV file (based on expected format)
 def datahandler(i, gesture, row_info):
-    base_dir = "Test_Data"  # Ensure this is the correct path
+    base_dir = "Output_Data"
 
     # Full path for the gesture directory
     gesture_dir = os.path.join(base_dir, gesture)
@@ -52,7 +54,7 @@ def datahandler(i, gesture, row_info):
 
 
 # Open the CSV file for reading
-with open("C:\\Users\\scott\\OneDrive\\Documents\\GitHub\\myo_ecn\\examples\\classification\\Test_Data\\BCICIV_2a_all_patients.csv",
+with open("C:\\Users\\scott\\OneDrive\\Documents\\GitHub\\myo_ecn\\examples\\classification\\Input_Data\\BCICIV_2a_all_patients.csv",
           'r') as file:
 
     # Set up readers
@@ -97,4 +99,4 @@ with open("C:\\Users\\scott\\OneDrive\\Documents\\GitHub\\myo_ecn\\examples\\cla
 
     # Notify
     total_time = time.time() - start_time
-    print(f"CSV file has been sorted. Please see Test_Data. Total time: {total_time:.4f} seconds.")
+    print(f"CSV file has been sorted. Please see Output_Data. Total time: {total_time:.4f} seconds.")
